c653a20c9dbbb7d2fc3f757372f8a8b9033e9ac5
Vinicius Assis <viniciusassis.dev@hotmail.com>
2023-09-27 20:50:15 -0300

    feat: create button component

---
+++ src/components/Atoms/Button/Button.tsx
@@ -0,0 +1,5 @@
+const Button = ({ content, className }: IButton) => {
+  return <button className={className}>{content}</button>
+}
+
+export default Button
---
+++ src/protocols/button.d.ts
@@ -0,0 +1,4 @@
+interface IButton  {
+  content: string
+  className?: string
+}
