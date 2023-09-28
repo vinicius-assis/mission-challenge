ff5361446052e706d11a6852cb5a4088d784d33b
Vinicius Assis <viniciusassis.dev@hotmail.com>
2023-09-28 19:46:55 -0300

    refactor: add states in Button component

--- src/components/Atoms/Button/Button.tsx
+++ src/components/Atoms/Button/Button.tsx
@@ -1,5 +1,19 @@
-const Button = ({ content, className }: IButton) => {
-  return <button className={className}>{content}</button>
+const colorSwitch = {
+  success: 'bg-green-500 hover:bg-green-600',
+  danger: 'bg-red-500 hover:bg-red-600',
+  info: 'bg-blue-500 hover:bg-blue-600'
+}
+
+const Button = ({ content, className, style, disabled, variant = 'info' }: IButton) => {
+  const buttonColor = colorSwitch[variant]
+
+  return <button
+    disabled={disabled}
+    style={style}
+    className={`py-1 px-3 rounded-sm ${buttonColor} ${className}`}
+    >
+      {content}
+    </button>
 }
 
 export default Button
--- src/protocols/button.d.ts
+++ src/protocols/button.d.ts
@@ -1,4 +1,7 @@
 interface IButton  {
   content: string
   className?: string
+  style?: React.CSSProperties
+  disabled?: boolean
+  variant?: 'success' | 'danger' | 'info'
 }
