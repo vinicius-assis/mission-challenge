9d385a0f621f6d11724876f15d35c7e5aef65b22
Vinicius Assis <viniciusassis.dev@hotmail.com>
2023-09-28 22:42:03 -0300

    refactor: add product label price component to replace html

--- src/components/Molecules/ProductControl/ProductControl.tsx
+++ src/components/Molecules/ProductControl/ProductControl.tsx
@@ -1,10 +1,11 @@
 import Button from "@/components/Atoms/Button/Button"
+import ProductPriceLabel from "@/components/Atoms/ProductPriceLabel/ProductPriceLabel"
 
 const ProductControl = ({ price, disabledRemove }: IProductControl) => {
   return (
     <div className="flex">
       <Button disabled={disabledRemove} content="-" variant="danger" className="mr-1 text-sm font-medium text-right" />
-      <p style={{ minWidth: '80px'}} className="mr-1 py-1 px-3 rounded-sm bg-gray-300 text-black text-sm font-medium text-right">{`R$${price}`}</p>
+      <ProductPriceLabel price={price} />
       <Button content="+" className="text-sm font-medium text-right" />
     </div>
   )
