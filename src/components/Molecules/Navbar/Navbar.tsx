55c05b17316915e4553c4567aaa9af0194222706
Vinicius Assis <viniciusassis.dev@hotmail.com>
2023-09-30 17:20:13 -0300

    feat: create a navbar

---
+++ src/components/Molecules/Navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import Button from "@/components/Atoms/Button/Button"
+import HeaderWrapper from "@/components/Atoms/HeaderWrapper/HeaderWrapper"
+import { CartContext } from "@/context/CartContext"
+import { usePathname } from "next/navigation"
+import { useContext } from "react"
+
+const Navbar = () => {
+  const pathName = usePathname()
+  const { totalItems } = useContext(CartContext)
+  console.log('totalItems', totalItems)
+
+  return (
+    <HeaderWrapper>
+      {pathName !== '/register' && <Button to="/register" content="Cadastrar Produto" />}
+      {pathName !== '/' && <Button to="/" content="Lista de Produtos" />}
+      {pathName !== '/cart' && <Button to="/cart" content={`Carrinho${totalItems ? `(${totalItems})` : ''}`} />}
+    </HeaderWrapper>
+  )
+}
+
+export default Navbar
--- src/components/Templates/ProvidersTemplate/ProvidersTemplate.tsx
+++ src/components/Templates/ProvidersTemplate/ProvidersTemplate.tsx
@@ -1,24 +1,16 @@
 'use client'
-import HeaderWrapper from '@/components/Atoms/HeaderWrapper/HeaderWrapper'
-import Button from '@/components/Atoms/Button/Button'
-import { usePathname } from 'next/navigation'
 import { ProductProvider } from '@/context/ProductContext'
 import { CartProvider } from '@/context/CartContext'
+import Navbar from '@/components/Molecules/Navbar/Navbar'
 
 const ProvidersTemplate: React.FC<{
   children: React.ReactNode
 }> = ({ children }) => {
-  const pathName = usePathname()
-
   return (
     <>
-      <HeaderWrapper>
-        {pathName !== '/register' && <Button to="/register" content="Cadastrar Produto" />}
-        {pathName !== '/' && <Button to="/" content="Lista de Produtos" />}
-        {pathName !== '/cart' && <Button to="/cart" content="Carrinho" />}
-      </HeaderWrapper>
       <ProductProvider>
         <CartProvider>
+          <Navbar />
           {children}
         </CartProvider>
       </ProductProvider>
