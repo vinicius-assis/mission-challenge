4f09569ce189cb2310b7d2ed2a79e9eaec986200
Vinicius Assis <viniciusassis.dev@hotmail.com>
2023-09-30 16:15:47 -0300

    feat: create a ProductForm organisms

--- src/components/Molecules/ProductInput/ProductInput.tsx
+++ src/components/Molecules/ProductInput/ProductInput.tsx
@@ -2,13 +2,24 @@ import { memo } from "react"
 import Input from "@/components/Atoms/Input/Input"
 import Button from "@/components/Atoms/Button/Button"
 
-const ProductInput = ({ onAppendInput, onRemoveItem, disabledRemove }: any) => {
+interface IInputData {
+  price: string
+  product: string
+}
+
+const ProductInput = ({ addProductField, disabledRemove, removeField, handleChange }: any) => {
+  const handleUpdateFields = (event: any) => {
+    const { target } = event
+    const { name, value } = target
+    handleChange(name, value)
+  }
+
   return (
     <div className="flex space-x-1 mb-2">
-      <Input name="product" label="Produto" />
-      <Input name="price" label="Preço" placeholder="0,00" currencySign="R$" />
-      <Button onClick={onRemoveItem} content="-" variant="danger" className="self-end h-9" disabled={disabledRemove} />
-      <Button onClick={onAppendInput} content="+" className="self-end h-9" />
+      <Input name="product" label="Produto" onChange={handleUpdateFields} />
+      <Input name="price" label="Preço" placeholder="0,00" currencySign="R$"  onChange={handleUpdateFields} />
+      <Button onClick={removeField} content="-" variant="danger" className="self-end h-9" disabled={disabledRemove} />
+      <Button onClick={addProductField} content="+" className="self-end h-9" />
     </div>
   )
 }
---
+++ src/components/Organisms/ProductForm/ProductForm.tsx
@@ -0,0 +1,96 @@
+import { useContext, useState } from "react"
+import { v4 as uuid } from 'uuid'
+
+import Button from "@/components/Atoms/Button/Button"
+import Loading from "@/components/Atoms/Loading/Loading"
+import ProductInput from "@/components/Molecules/ProductInput/ProductInput"
+import { ProductContext } from "@/context/ProductContext"
+
+const ProductForm = () => {
+  const [products, setProducts] = useState([{
+    id: uuid(),
+    product: '',
+    price: ''
+  }])
+  const { dispatch } = useContext(ProductContext)
+
+  const [registerLoading, setRegisterLoading] = useState(false)
+  const [successMessage, setSuccessMessage] = useState(false)
+
+  const productLabel = `Produto${products?.length > 1 ? 's' : ''}`
+
+  const handleAddProductField = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault()
+    setProducts((oldValue: any) => [...oldValue, {
+      id: uuid(),
+      product: '',
+      price: ''
+    }])
+  }
+
+  const handleRemoveProductField = (event: React.MouseEvent<HTMLElement>, itemIndex: number) =>  {
+    event.preventDefault()
+    setProducts((oldValue: any) => {
+      const filteredValues = oldValue.filter((_: unknown, index: number) => index !== itemIndex)
+      return filteredValues
+    })
+  }
+
+  const handleUpdateName = (index: number) => (field: 'name' | 'price', value: string) => {
+    const newProducts = [...products]
+    newProducts[index] = {
+      ...products[index],
+      [field]: value
+    }
+    setProducts(newProducts)
+  }
+
+  const handleRegisterProducts = (event: React.FormEvent) => {
+    event.preventDefault()
+    setRegisterLoading(true)
+    try {
+      const filteredProducts = products?.filter((item) => item.price && item.product)
+      dispatch({ type: 'REGISTER_PRODUCTS', payload: filteredProducts })
+      setSuccessMessage(true)
+    } catch (error) {
+      throw `An error occurred: ${error}`
+    }
+    setTimeout(() => {
+      setRegisterLoading(false)
+    }, 1000)
+  }
+
+  if (successMessage) {
+    return (
+      <div className="container">
+        <h4 className="text-base font-semibold mb-4 text-center">Lista cadastrada com sucesso!</h4>
+        <div className="flex space-x-4 w-full justify-center">
+          <Button to="/" content="Voltar para lista" />
+          <Button onClick={() => setSuccessMessage(false)} variant="success" content="Cadastrar mais produtos" />
+        </div>
+      </div>
+    )
+  }
+
+
+  return registerLoading ? <Loading content={`Cadastrando ${productLabel}`} /> : (
+    <form>
+      {products?.map((_, index: number, array) => (
+        <ProductInput
+          key={index}
+          index={index}
+          addProductField={handleAddProductField}
+          removeField={handleRemoveProductField}
+          updateProducts={setProducts}
+          disabledRemove={array.length === 1}
+          handleChange={handleUpdateName(index)}
+        />
+      ))}
+      <div className="flex justify-end">
+        <Button onClick={handleRegisterProducts} type="submit" variant="success" content={`Cadastrar ${productLabel}`} />
+      </div>
+    </form>
+  )
+}
+
+export default ProductForm
